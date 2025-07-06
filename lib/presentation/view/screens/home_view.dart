513a8b9c0431528824d41b9d3335889b1fd32049
tasneem-gamal <tasneemgamal044@gmail.com>
2025-07-06 12:15:58 +0200

    feat: create home app bar

--- lib/presentation/view/screens/home_view.dart
+++ lib/presentation/view/screens/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:streaky/core/theming/styles.dart';
+import 'package:streaky/core/utils/constants.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -16,6 +18,29 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: Constants.appPadding,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            automaticallyImplyLeading: false,
+            expandedHeight: 70,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Good morning, \nDiana',
+                style: CustomTextStyles.font16BlackBold(context),
+              ),
+            ),
+            actions: [
+              IconButton(
+                onPressed: (){}, 
+                icon: Icon(Icons.notifications)
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
